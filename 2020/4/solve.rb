15c5cd2cf57076daa3025031226d64224d20f7ff
Alex Quach <alexhquach@gmail.com>
2020-12-07 18:09:35 -0800

    4.1

--- 2020/4/solve.rb
+++ 2020/4/solve.rb
@@ -1,1 +1,12 @@
-File.readlines('input.txt')
+require 'set'
+
+KEYS = %w(byr iyr eyr hgt hcl ecl pid).to_set
+
+puts File.read('input.txt')
+  .gsub(/\n([a-z])/, ' \1')
+  .split("\n")
+  .count { |l|
+    keys = l.split(' ').map { |kv| kv.split(':').first }.to_set
+
+    (KEYS - keys).empty?
+  }
